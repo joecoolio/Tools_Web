6f3e3b91196e0b66925f327fae983605a77a666b
Mike Billings <3169366+joecoolio@users.noreply.github.com>
2025-08-19 08:27:01 -0400

    Deleted outdated main component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,4 @@ export const routes: Routes = [
 
     {path: 'register', loadComponent: () => import('./user/register/register.component').then(m => m.RegisterComponent), title: "Register"},
     {path: 'login', loadComponent: () => import('./user/login/login.component').then(m => m.LoginComponent), title: "Login"},
-    {path: 'main', loadComponent: () => import('./main/main.component').then(m => m.MainComponent), title: "Map"},
 ];
